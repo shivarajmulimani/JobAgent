5c44fdfb1f17df6d1e54aa1357187b9de642f88e
unknown <shivmantu@gmail.com>
2025-02-16 01:57:30 +0530

    added analyse with resume page

--- archestration/master.py
+++ archestration/master.py
@@ -42,11 +42,40 @@ class AgentArchestration:
         except Exception as e:
             print("Failed to execute the agent - ", e)
 
+    def job_match_individual(self, id):
+        try:
+            response = dict()
+            # Filter by a specific index
+            row = self.jobs[self.jobs['id'] == id]  # Get all columns for index
+            match_response = None
+            resume_update_response = None
+            job_description = row['description'].values[0]
+            job_url = row['job_url'].values[0]
+            title = row['title'].values[0]
+            company = row['company'].values[0]
+            match_response = self.job_matching_agent.run_agent(job_description)
+            if match_response:
+                response['rating'] = match_response.content.rating
+                response['justification'] = match_response.content.justification
+                resume_update_response = self.resume_updating_agent.run_agent(match_response.content.justification)
+                response['updated resume'] = resume_update_response.content
+                filename = str(uuid.uuid4()) + ".txt"
+                write_to_file(filename, resume_update_response.content)
+        except Exception as e:
+            print(e)
+        return response
+
+    def job_match_bulk(self):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+
     def run_agent(self, resume_justification):
         try:
             response = None
             if resume_justification:
-                response = self.execute_agent(resume_justification)
+                self.execute_agents(resume_justification)
         except Exception as e:
             print("Failed to run resume updating agent - ", e)
         return response
@@ -55,6 +84,7 @@ if __name__ == "__main__":
     with open(SC.RESUME_DATA_FILE, 'r') as file:
         resume = file.read()
     jobs = pd.read_json(SC.SCRAPED_JOBS_DATA_FILE_JSON)
-
     jma = AgentArchestration(resume, jobs)
-    jma.execute_agents()
+    # jma.execute_agents()
+    response = jma.job_match_individual("gd-1009615110820")
+    print(response)
