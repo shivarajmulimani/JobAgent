37871fadfd544290dfc7aae31e817d326d72613a
unknown <shivmantu@gmail.com>
2025-02-22 20:11:24 +0530

    Added intro page

--- utils/stringcontants.py
+++ utils/stringcontants.py
@@ -1,6 +1,6 @@
 import os
 
-SCRAPED_JOBS_DATA_FILE_JSON = os.path.dirname(os.path.abspath(__file__)) + '/../data/jobs/jobs1.json'
-SCRAPED_JOBS_DATA_FILE_CSV = os.path.dirname(os.path.abspath(__file__)) + '/../data/jobs/jobs1.csv'
+SCRAPED_JOBS_DATA_FILE_JSON = os.path.dirname(os.path.abspath(__file__)) + '/../data/jobs/jobs.json'
+SCRAPED_JOBS_DATA_FILE_CSV = os.path.dirname(os.path.abspath(__file__)) + '/../data/jobs/jobs.csv'
 RESUME_DATA_FILE = os.path.dirname(os.path.abspath(__file__)) + '/../data/resume/resume_content.txt'
 UPDATED_RESUME_STORAGE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../data/updated_resumes/'
