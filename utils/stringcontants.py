9ed46be1c5e83e5558d7715e78f7a4e8d51ffd2f
unknown <shivmantu@gmail.com>
2025-03-01 23:54:31 +0530

    updated files

--- utils/stringcontants.py
+++ utils/stringcontants.py
@@ -4,6 +4,7 @@ SCRAPED_JOBS_DATA_FILE_JSON = os.path.dirname(os.path.abspath(__file__)) + '/../
 SCRAPED_JOBS_DATA_FILE_CSV = os.path.dirname(os.path.abspath(__file__)) + '/../data/jobs/jobs.csv'
 RESUME_DATA_FILE = os.path.dirname(os.path.abspath(__file__)) + '/../data/resume/resume_content.txt'
 UPDATED_RESUME_STORAGE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../data/updated_resumes/'
+UPDATED_RESUME_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../data/updated_resumes/updated_file.pdf'
 
 README_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../README.md'
 README_IMAGE_1_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../app_image.png'
