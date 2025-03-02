d6473f8f0f3643e036c07b3dec56075c70f8af2c
unknown <shivmantu@gmail.com>
2025-03-02 23:28:35 +0530

    Added feature to support multi format files

--- utils/generic_utility.py
+++ utils/generic_utility.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import pdfkit
+import streamlit as st
+import fitz  # PyMuPDF for PDFs
+from docx import Document
+from io import BytesIO
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from utils import stringcontants as SC
@@ -25,3 +29,38 @@ def save_html_to_pdf(html_content, pdf_path):
 
     except Exception as e:
         print("Failed to save updated resume - ", e)
+
+
+def extract_text_from_file(uploaded_file):
+    text_content = ""
+    try:
+        if uploaded_file is not None:
+            file_extension = uploaded_file.name.split(".")[-1].lower()
+
+            if file_extension == "pdf":
+                text_content = extract_text_from_pdf(uploaded_file)
+            elif file_extension == "docx":
+                text_content = extract_text_from_docx(uploaded_file)
+            elif file_extension == "txt":
+                text_content = extract_text_from_txt(uploaded_file)
+            else:
+                st.error("Unsupported file format. Please upload a .txt, .pdf, or .docx file.")
+                return None
+        return text_content
+    except Exception as e:
+        print("failed to extract text from file - ", e)
+
+
+def extract_text_from_pdf(uploaded_file):
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    return "\n".join([page.get_text() for page in doc])
+
+
+def extract_text_from_docx(uploaded_file):
+    doc = Document(BytesIO(uploaded_file.read()))
+    return "\n".join([para.text for para in doc.paragraphs])
+
+
+def extract_text_from_txt(uploaded_file):
+    return uploaded_file.read().decode("utf-8")
+
